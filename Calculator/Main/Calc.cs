55cd5ea3b37cd18e39c465084f8b427f7028ab0c
Rajat <thakurrajat201@gmail.com>
2021-05-28 16:23:50 +0530

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   Calculator.sln
    	new file:   Calculator/Calculator.csproj
    	new file:   Calculator/Main/Calc.cs
    	new file:   Calculator/Program.cs
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.deps.json
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.dll
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.exe
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.pdb
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.runtimeconfig.dev.json
    	new file:   Calculator/bin/Debug/netcoreapp3.1/Calculator.runtimeconfig.json
    	new file:   Calculator/obj/Calculator.csproj.nuget.dgspec.json
    	new file:   Calculator/obj/Calculator.csproj.nuget.g.props
    	new file:   Calculator/obj/Calculator.csproj.nuget.g.targets
    	new file:   Calculator/obj/Debug/netcoreapp3.1/.NETCoreApp,Version=v3.1.AssemblyAttributes.cs
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.AssemblyInfo.cs
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.AssemblyInfoInputs.cache
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.assets.cache
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.csproj.CoreCompileInputs.cache
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.csproj.FileListAbsolute.txt
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.csprojAssemblyReference.cache
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.dll
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.genruntimeconfig.cache
    	new file:   Calculator/obj/Debug/netcoreapp3.1/Calculator.pdb
    	new file:   Calculator/obj/Debug/netcoreapp3.1/apphost.exe
    	new file:   Calculator/obj/project.assets.json
    	new file:   Calculator/obj/project.nuget.cache
    
    Untracked files:
    	.vs/

---
+++ Calculator/Main/Calc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Main
+{
+  static  class Calc
+    {
+        static int a, b,e=1;
+        static string c, d;
+       public static void Details()
+        {
+
+            Again:
+            Console.WriteLine("Write the first number");
+            c = Console.ReadLine();
+            if(int.TryParse(c, out e))
+            {
+                a = Int32.Parse(c);
+            }
+            else
+            {
+                Console.WriteLine("You have entered wrong value");
+                goto Again;
+            }
+            Again2:
+            Console.WriteLine("Write the Second no");
+            d = Console.ReadLine();
+
+            if (int.TryParse(d, out e))
+            {
+                b = Int32.Parse(d);
+            }
+            else
+            {
+                Console.WriteLine("You have entered wrong value");
+                goto Again2;
+            }
+       
+        }
+       public static int Addition()
+        {
+            return a + b;
+        }
+        public static int Sub()
+        {
+            return a - b;
+        }
+        public static float divide()
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Calculator.Main;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            top:
+            string symbol;
+            int total;
+            float ans;
+            Console.WriteLine("What kind of calculation do you want to perform??(+, -, * or /)");
+            symbol = Console.ReadLine();
+            if (symbol == "+")
+            {
+                Calc.Details();
+              total=  Calc.Addition();
+                Console.WriteLine(total);
+            }
+            else if(symbol == "-")
+            {
+                Calc.Details();
+                total = Calc.Sub();
+                Console.WriteLine(total);
+            }
+            else if (symbol == "/")
+            {
+                Calc.Details();
+                ans = Calc.divide();
+                Console.WriteLine(ans);
+            }
+            else
+            {
+                Console.WriteLine("Your input is wrong");
+                goto top;
+            }
+
+
+        }
+    }
+
+
+     
+}
